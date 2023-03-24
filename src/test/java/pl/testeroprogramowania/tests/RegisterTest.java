e127ec638f2c791b4b153ed1f0dc88b4906746f6
Marcin Chmielowiec <marcinchmielow@gmail.com>
2023-03-24 17:31:20 +0100

    Create new project Add dependencies to pom.xml Add DriverFactory class Add BaseTest class Add RegisterTest class Modify .gitignore

---
+++ src/test/java/pl/testeroprogramowania/tests/RegisterTest.java
@@ -0,0 +1,10 @@
+package pl.testeroprogramowania.tests;
+
+import org.testng.annotations.Test;
+
+@Test
+public class RegisterTest extends BaseTest {
+    public void registerUserTest(){
+        System.out.println("Mock");
+    }
+}
---
+++ src/test/java/pl/testeroprogramowania/utils/DriverFactory.java
@@ -0,0 +1,15 @@
+package pl.testeroprogramowania.utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class DriverFactory {
+
+    public static WebDriver getDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        return new ChromeDriver(options);
+    }
+}
